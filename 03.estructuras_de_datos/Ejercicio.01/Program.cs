23e0deb8af92727bcaf4aad35da9d37535687c12
Gabriel Lanza <gabrieldaniel.lanza@gmail.com>
2022-08-18 15:23:25 -0300

    Ejercicios sesión 3 : Ejercicio 1

---
+++ 03.estructuras_de_datos/Ejercicio.01/Program.cs
@@ -0,0 +1,58 @@
+﻿
+Cliente cliete = new Cliente()
+{
+    NombreCompleto = "Gabriel Lanza",
+    Telefono = "(11) 6252-0101",
+    Email = "gab@gmail.com",
+    EsCliente = true,
+    Direccion = new Direccion()
+    {
+        Altura = 861,
+        Calle = "Dante Alighieri",
+        Localidad = "Avellandeda",
+        Provincia = "Buenos Aires"
+    }
+};
+
+Console.WriteLine(cliete);
+
+
+public struct Direccion
+{
+    public string Calle;
+    public int Altura;
+
+    public string Localidad;
+    public string Provincia;
+
+    public override string ToString()
+    {
+        return string.Format("{0}, {1}, {2}, {3}", Calle, Altura, Localidad, Provincia);
+    }
+
+}
+
+public struct Cliente
+{
+
+    public Cliente(string nombreCompleto, string telefono, Direccion direccion, string email, bool esCliente)
+    {
+        this.NombreCompleto = nombreCompleto;
+        this.Telefono = telefono;
+        this.Direccion = direccion;
+        this.Email = email;
+        this.EsCliente = esCliente;
+    }
+
+    public string NombreCompleto;
+    public string Telefono;
+    public Direccion Direccion;
+    public string Email;
+    public bool EsCliente;
+
+    public override string ToString()
+    {
+        return string.Format("{0}, {1}, {2}, {3}, {4}", NombreCompleto, Telefono, Direccion, Email, EsCliente);
+    }
+
+}
