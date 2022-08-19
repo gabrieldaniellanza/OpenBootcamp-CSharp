bf815739b0e38f75652aa688609921ebf587a637
Gabriel Lanza <gabrieldaniel.lanza@gmail.com>
2022-08-19 17:54:25 -0300

    05 BUCLES 2: ejercicios

---
+++ 05.bucles.II/Ejercicio.01.IF/05.Bucles.Ejercicio.01.IF/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Ejercicio 1 - If
+
+Escribe un programa que:
+Pida datos a un cliente: Nombre, email, cupón
+Determine si un cliente tiene un cupon descuento
+Muestre un precio rebajado en función del descuento
+Muestre el precio de un producto sin descuento si no hay cupón
+Nota: puedes poner un precio fijo de un producto o uno variable.
+ * */
+
+decimal precio = 100;
+
+Dictionary<string, decimal> cupones = new Dictionary<string, decimal>();
+cupones.Add("CUPON_DIEZ", (decimal)0.10);
+cupones.Add("CUPON_AUTONOMO", (decimal)0.155);
+cupones.Add("CUPON_VIP", (decimal) 0.204);
+
+
+Console.WriteLine("Ingrese su nombre: ");
+string? nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese su e-mail: ");
+string? email = Console.ReadLine();
+
+Console.WriteLine("Ingrese su cupon: ");
+string? cupon = Console.ReadLine();
+
+decimal descuento = 0;
+if (cupon != null && cupones.ContainsKey(cupon))
+{
+    cupones.TryGetValue(cupon, out descuento);
+}
+
+Console.WriteLine($"El producto le contará: $ {precio * (1 - descuento)}");
---
+++ 05.bucles.II/Ejercicio.02.SWITCH/05.Bucles.Ejercicio.02.SWITCH/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Ejercicio 2 - Switch
+
+Haz una lista de lenguajes de programación, 
+por ejemplo: C#, Java, C++. 
+Presenta la lista en consola y pide que el usuario seleccione el lenguaje mediante 
+1, 2, 3 o a, b, c. 
+Presenta el resultado en consola.
+
+Nota: puedes añadir al resultado el “Hola, mundo” para el caso de C#.
+*/
+
+
+List<string> ListaDeLenguajes = new List<string>() 
+{
+    "C#", "Java", "C++", "VB .net", "Python", "JavaScript"
+};
+
+
+Console.WriteLine("LENGUAJES:");
+
+foreach ( string lenguaje in ListaDeLenguajes)
+    Console.WriteLine($"{ListaDeLenguajes.IndexOf(lenguaje)+1} - {lenguaje}");
+
+Console.WriteLine($"{Environment.NewLine}Selecciona un lenguaje:");
+string? seleccion = Console.ReadLine();
+int select;
+string lenjuaje;
+
+if (int.TryParse(seleccion, out select)) 
+{
+    // opcion sin switch
+    //seleccion = $"Usted seleccionó : {ListaDeLenguajes[select - 1]}";
+
+    // opcion con switch
+    switch (select)
+    {
+        case 1:
+            seleccion = $"Usted seleccionó : {ListaDeLenguajes[0]}";
+            break;
+
+        case 2:
+            seleccion = $"Usted seleccionó : {ListaDeLenguajes[1]}";
+            break;
+
+        case 3:
+            seleccion = $"Usted seleccionó : {ListaDeLenguajes[2]}";
+            break;
+
+        case 4:
+            seleccion = $"Usted seleccionó : {ListaDeLenguajes[3]}";
+            break;
+
+        case 5:
+            seleccion = $"Usted seleccionó : {ListaDeLenguajes[4]}";
+            break;
+
+        case 6:
+            seleccion = $"Usted seleccionó : {ListaDeLenguajes[5]}";
+            break;
+
+        default:
+            seleccion = "Opcion invalida";
+            break;
+
+    }
+
+    Console.WriteLine(seleccion);
+}
+
+
+
+
