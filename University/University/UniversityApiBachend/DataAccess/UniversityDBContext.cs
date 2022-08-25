5faff71c51d1a41bf767086149a4f0984e202987
Gabriel Lanza <gabrieldaniel.lanza@gmail.com>
2022-08-24 22:07:58 -0300

    Proyecto University

---
+++ University/University/UniversityApiBachend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBachend.Models.DataModels;
+
+namespace UniversityApiBachend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) { 
+        
+        }
+
+        // TODO: agregar DBSETs (tablas de nuestra base de datos)
+        public DbSet<User>? Users { get; set; }
+
+        public DbSet<Curso>? Cursos { get; set; }
+    }
+}
---
+++ University/University/UniversityApiBachend/Models/DataModels/Curso.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBachend.Models.DataModels
+{
+    public enum Niveles
+    {
+        Basico,
+        Intermedio,
+        Avanzado
+    }
+
+    public class Curso : BaseEntity
+    {
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string DescripcionLarga { get; set; } = string.Empty;
+
+        [Required]
+        public string PublicoObjetivo{ get; set; } = string.Empty;
+
+        [Required]
+        public string Objetivos { get; set; } = string.Empty;
+        [Required]
+        public string Requisitos { get; set; } = string.Empty;
+
+        [Required]
+        public Niveles Nivel { get; set; }
+    }
+}
